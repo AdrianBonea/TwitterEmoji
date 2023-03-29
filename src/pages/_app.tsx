ba10c5b021cfbd1bfe66be5efa8bd0a0cb260dcc
Adrian <adrian.bonea@setmobile.ro>
2023-03-29 09:58:55 +0300

    Types safe and started working on style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import "y/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
